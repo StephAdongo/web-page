e936654b3eab73cb5325d65915203d7821459add
StephAdongo <stephanie@kabarak.ac.ke>
2024-05-17 16:20:31 +0300

    add index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+const quote = document.querySelector(".quote");
+quote.onclick = () => alert("Thank you for visiting our page");
+const quote2 = document.querySelector(".quote2");
+quote2.onclick = () => alert ("Thank you for shopping with us,we also deliver worldwide.");
+const quote3 = document.querySelector(".quote3");
+quote3.onclick = () => alert("T-shirts,Apparel,Paper,Merch");
